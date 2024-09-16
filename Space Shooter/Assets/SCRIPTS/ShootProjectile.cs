b3be2a82b65225f1daf3e913c08b08f646576e2b
anwrat <anweshrwt22@gmail.com>
2024-09-16 21:59:50 +0545

    Shooting Enemies and destroying them
    Added projectile prefabs to spawn and destroy enemies on collide. Also added cooldown between shooting projectiles through Coroutine.

---
+++ Space Shooter/Assets/SCRIPTS/ShootProjectile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+    public GameObject projectileprefab;
+    private bool readytoshoot = true; //Boolean value to check projectile cooldown
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Under Unity Edit, inside project setting we can change default buttons through input manager
+        if (readytoshoot && Input.GetButtonDown("Fire1")) { //If projectile is not under cooldown time(Coroutine)
+            //Spawn the projectile at the position of the object script is attached to
+            readytoshoot = false;//set cooldown for projectile 
+            StartCoroutine(waitbetweenshots());
+            Instantiate(projectileprefab,transform.position,Quaternion.identity);//Quaternion.identity means no rotation
+        }
+    }
+    //Coroutine function to wait for cetain seconds before being able to shoot again
+    IEnumerator waitbetweenshots()
+    {
+        yield return new WaitForSeconds(1);
+        readytoshoot=true;
+    }
+}
