1d2074a086afdd74267055ebb255c0aad3eff2f1
anwrat <anweshrwt22@gmail.com>
2024-09-17 21:48:31 +0545

    Player health UI + destroy enemy ships when they reach bottom of screen
    Added UI image and text for player health, added a script that reduces player lives count every time enemy ship touches player and after 0 lives player ship is destroyed (Not Complete)

--- Space Shooter/Assets/SCRIPTS/EnemyShipMovement.cs
+++ Space Shooter/Assets/SCRIPTS/EnemyShipMovement.cs
@@ -24,5 +24,9 @@ public class EnemyShipMovement : MonoBehaviour
             transform.position = new Vector3(transform.position.x,transform.position.y-1,transform.position.z);
             moveSpeed *= -1;//if the original movespeed is 5, it changes to -5 changing the move direction
         }
+        if(collision.gameObject.tag == "LowerBoundary")
+        {
+            Destroy(gameObject);
+        }
     }
 }
