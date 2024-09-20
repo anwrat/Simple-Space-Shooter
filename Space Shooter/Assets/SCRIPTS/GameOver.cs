723c46f8f315a0c6e32ddcf84c22c6784146366a
anwrat <anweshrwt22@gmail.com>
2024-09-21 05:43:12 +0545

    Blinking text
    Used coroutine to make a textmeshpro UI text blink on a given number of time

--- Space Shooter/Assets/SCRIPTS/GameOver.cs
+++ Space Shooter/Assets/SCRIPTS/GameOver.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
     public GameObject gameoverpanel;
     public GameObject playership;
+    public TMP_Text blinktext;
+    private float blinktime = 0.5f;
+    private bool isBlinking = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +24,11 @@ public class GameOver : MonoBehaviour
         if (gameoverpanel.activeSelf)
         {
             Debug.Log("Game over panel is active");
-
+            if (!isBlinking)
+            {
+                StartCoroutine(BlinkText());
+                isBlinking = true;
+            }
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 Debug.Log("Enter key pressed");
@@ -41,5 +49,15 @@ public class GameOver : MonoBehaviour
         Debug.Log("Reloading the scene");
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Coroutine to blink the text
+    IEnumerator BlinkText()
+    {
+        while (gameoverpanel.activeSelf) // Blink only when game over panel is active
+        {
+            blinktext.enabled = !blinktext.enabled; // Toggle the visibility
+            yield return new WaitForSeconds(blinktime); // Wait for the blink interval
+        }
     }
 }
